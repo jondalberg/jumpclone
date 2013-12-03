f6e9d269448237c0e5fd475a45037db693cbc950
Jon Dalberg <jon.dalberg@gmail.com>
2013-12-02 19:11:10 -0600

    use 1 sound

--- public/js/game.js
+++ public/js/game.js
@@ -314,9 +314,9 @@ function SoundPool(maxSize) {
   };
 }
 
-var bounce_sounds = new SoundPool(10);
+var bounce_sounds = new SoundPool(1);
 //bounce_sounds.init('bounce');
-var big_bounce_sounds = new SoundPool(5);
+var big_bounce_sounds = new SoundPool(1);
 //big_bounce_sounds.init('big_bounce');
 
 var sounds_loaded = false;
